b80a38be5f26e16feb2374dd3197ac377646ef31
Lucas Viana <oviana@outlook.com>
2021-11-27 20:09:46 -0300

    Extract connection-related methods to class

--- src/app/connection.py
+++ src/app/connection.py
@@ -1,12 +1,43 @@
-#!/usr/bin/python
 from configparser import ConfigParser
 
-def config(filename='database.ini', section='postgresql'):
-    parser = ConfigParser()
-    parser.read(filename)
+import psycopg2
 
-    if not parser.has_section(section):
-        raise Exception('Section {0} not found in {1} file'.format(section, filename))
 
-    params = parser.items(section)
-    return { param[0]: param[1] for param in params }
+class Connection:
+    def __init__(self):
+        self.connection, self.cursor = self.connect()
+
+    def __config(self, filename="database.ini", section="postgresql"):
+        parser = ConfigParser()
+        parser.read(filename)
+
+        if not parser.has_section(section):
+            raise Exception(f"Section {section} not found in {filename}.")
+
+        # params = parser.items(section)
+        # # return {param[0]: param[1] for param in params}
+
+        return dict(parser.items(section))
+
+    def connect(self):
+        params = self.__config()
+
+        # connects to the database
+        print("Connecting to the PostgreSQL database...")
+        connection = psycopg2.connect(**params)
+
+        # create a cursor
+        cursor = connection.cursor()
+
+        # TODO: remove this after development
+        # displays the db server's version
+        print("PostgreSQL database version:")
+        cursor.execute("SELECT version()")
+        print(cursor.fetchone())
+
+        return connection, cursor
+
+    def disconnect(self):
+        if self.connection:
+            self.connection.close()
+            print("Connection closed")
