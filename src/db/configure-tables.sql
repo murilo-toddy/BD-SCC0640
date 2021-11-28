e08056778b1d71ca818c9c506ac12a7714047c25
murilo-toddy <murilo.todao@usp.br>
2021-11-28 17:15:35 -0300

    Creates talk insertion script

--- src/db/configure-tables.sql
+++ src/db/configure-tables.sql
@@ -5,3 +5,10 @@
 \i db/db/data/university.sql
 \i db/db/data/residence.sql
 \i db/db/data/party.sql
+\i db/db/data/talk.sql
+
+-- INFORMAÇÕES RESTANTES:
+-- Tabela CURSANDO
+-- Informações Adicionais RESIDENCIA
+-- Relacionamentos de RESPONSABILIDADE, ALUGUEL e VENDA
+-- Open Bar FESTA
