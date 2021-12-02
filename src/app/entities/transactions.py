c949aaaf24f8bee2ead513dd8a2d4cbb10954690
Lucas Viana <oviana@outlook.com>
2021-12-01 21:29:12 -0300

    Create methods for querying rents by tenant and responsible

--- src/app/entities/transactions.py
+++ src/app/entities/transactions.py
@@ -7,6 +7,36 @@ class RentContract:
     def query_by_tenant(cpf: str):
         assert_regex(cpf, regexes.cpf, "cpf")
 
-        query = "SELECT inicio, residencia, fim, aluguel FROM contrato_aluguel \
-                WHERE locatario IS %s"
-        return Connection().exec_commit(query, cpf, lambda cur: cur.fetchall())
+        query = """
+            SELECT inicio, residencia, fim, aluguel FROM contrato_aluguel
+            WHERE locatario = %s
+        """
+        return Connection().exec_commit(query, cpf, cb=lambda cur: cur.fetchall())
+
+    @staticmethod
+    def query_by_tenant_join_residence(cpf: str):
+        assert_regex(cpf, regexes.cpf, "cpf")
+
+        query = """
+            SELECT endereço, cidade, estado, inicio, fim, C.aluguel, condominio,
+            coletividade
+            FROM contrato_aluguel as C
+            INNER JOIN residencia as R on C.residencia = R.id
+            LEFT JOIN imovel as I ON R.id = I.id
+            WHERE C.locatario = %s
+        """
+        return Connection().exec_commit(query, cpf, cb=lambda cur: cur.fetchall())
+
+    @staticmethod
+    def query_by_responsible_join_residence(cpf: str):
+        assert_regex(cpf, regexes.cpf, "cpf")
+
+        query = """
+            SELECT endereço, cidade, estado, inicio, fim, C.aluguel, condominio,
+            coletividade
+            FROM contrato_aluguel as C
+            INNER JOIN residencia as R on C.residencia = R.id
+            LEFT JOIN imovel as I ON R.id = I.id
+            WHERE C.responsavel = %s
+        """
+        return Connection().exec_commit(query, cpf, cb=lambda cur: cur.fetchall())
--- src/app/main.py
+++ src/app/main.py
@@ -1,29 +1,38 @@
 from connection import Connection
 from entities.people import Person, Professor, Responsible, Student
 from entities.residences import Property, Residence
+from entities.transactions import RentContract
 from models import Address, State
+from pprint import pp
 
 if __name__ == "__main__":
-    Person(
-        cpf="11471634990",
-        rg="137229919",
-        name="tod",
-        birthdate="27/08/2001",
-        roles=[
-            Student("11471634990", True, False),
-            Professor("11471634990", "Sistemas de Recomendação"),
-            Responsible("11471634990"),
-        ],
-    ).insert_db()
+    # Person(
+    #     cpf="11471634990",
+    #     rg="137229919",
+    #     name="tod",
+    #     birthdate="27/08/2001",
+    #     roles=[
+    #         Student("11471634990", True, False),
+    #         Professor("11471634990", "Sistemas de Recomendação"),
+    #         Responsible("11471634990"),
+    #     ],
+    # ).insert_db()
 
-    Residence(
-        45234.526,
-        Address(State["SP"], "São Carlos", "13561130", "Teste"),
-        Property(240000, 380, True),
-        2,
-        2,
-        70,
-        10,
-    ).insert_db()
+    # Residence(
+    #     45234.526,
+    #     Address(State["SP"], "São Carlos", "13561130", "Teste"),
+    #     Property(240000, 380, True),
+    #     2,
+    #     2,
+    #     70,
+    #     10,
+    # ).insert_db()
+
+    res = [
+        RentContract.query_by_tenant_join_residence("89738378001"),
+        # RentContract.query_by_responsible_join_residence("89738378001")
+    ]
+    for r in res:
+        pp(r)
 
     Connection().disconnect()
