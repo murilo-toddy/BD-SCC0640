0065296b7391cd732367196b4176b6e44e97fef7
Lucas Viana <oviana@outlook.com>
2021-12-04 16:00:30 -0300

    Create login

--- src/app/main.py
+++ src/app/main.py
@@ -3,37 +3,45 @@ import menu
 from connection import Connection
 from enums import PersonPermissions
 from state import CurrentUser
-from utils import prompt_menu
+from utils import prompt, prompt_menu, regexes
 
-current_user = CurrentUser()
-
-permissions: list[PersonPermissions] = current_user.fetch_permissions()
-
-
-def permission_match(functionality):
-    return bool([p for p in functionality["permissions"] if p.value in permissions])
-
-
-print(
-    f"Olá, {current_user.fetch_name().split(' ')[0]}! Tudo bem? Seja bem vind@ "
-    + "ao Sistema de Monitoramento da Vida Universitária.",
-    end="",
+cpf = prompt(
+    "\nOlá! Tudo bem?\nPara continuar, for favor, forneça o seu CPF:",
+    regexes.cpf.match,
 )
 
-options = [func["option"] for func in menu.menu if permission_match(func)]
-functions = [func["function"] for func in menu.menu if permission_match(func)] + [
-    functionalities.quit
-]
+try:
+    current_user = CurrentUser(cpf)
+except Exception as error:
+    current_user = None
+    print("\nHouve um erro!", error, end="\n\n")
 
-while True:
-    print("\n\n---------------------------------------------------\n")
-    if options:
-        i = prompt_menu(options, menu.leading_text, menu.trailig_text, True)
-        functions[i]()
-    else:
-        print(
-            "Infelizmente você não possui nenhuma funcionalidade disponível.\nAté logo!\n"
-        )
-        break
+if current_user:
+    permissions: list[PersonPermissions] = current_user.fetch_permissions()
 
-Connection().disconnect()
+    def permission_match(functionality):
+        return bool([p for p in functionality["permissions"] if p.value in permissions])
+
+    print(
+        f"Olá, {current_user.fetch_name().split(' ')[0]}! Tudo bem? Seja bem vind@ "
+        + "ao Sistema de Monitoramento da Vida Universitária.",
+        end="",
+    )
+
+    options = [func["option"] for func in menu.menu if permission_match(func)]
+    functions = [func["function"] for func in menu.menu if permission_match(func)] + [
+        functionalities.quit
+    ]
+
+    while True:
+        print("\n\n---------------------------------------------------\n")
+        if options:
+            i = prompt_menu(options, menu.leading_text, menu.trailig_text, True)
+            functions[i]()
+        else:
+            print(
+                "Infelizmente você não possui nenhuma funcionalidade disponível.\nAté logo!\n"
+            )
+            break
+
+    Connection().disconnect()
--- src/app/state.py
+++ src/app/state.py
@@ -1,16 +1,24 @@
 from entities.people import Person
+from utils import assert_regex, regexes, remove_symbols
 
 
 class CurrentUser:
     """Singleton for getting the current user's info."""
 
-    __cpf = "61520188030"
-
     # Guarantees the CurrentUser class is a Singleton
-    def __new__(cls):
+    def __new__(cls, cpf=None):
         if not hasattr(cls, "instance"):
             cls.instance = super(CurrentUser, cls).__new__(cls)
         return cls.instance
+
+    def __init__(self, cpf: str = None):
+        if cpf is not None:
+            assert_regex(cpf, regexes.cpf)
+            self.__cpf = remove_symbols(cpf)
+
+            user_exists = Person.query_by_cpf(self.__cpf)
+            if not user_exists:
+                raise Exception('Não existe um usuário com esse CPF.')
 
     def get_cpf(self):
         return self.__cpf
