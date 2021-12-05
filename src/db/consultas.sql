baeaaa6d262c0ccf5debe5bf659c8665fb9c5544
murilo-toddy <murilo.todao@usp.br>
2021-12-05 13:53:37 -0300

    Consultas formatting

--- src/db/consultas.sql
+++ src/db/consultas.sql
@@ -1,9 +1,9 @@
 -- QUERY 1
 
 SELECT p.nome, COUNT(i.comprador) as festas, c.curso
-FROM ingresso i JOIN cursando c ON c.aluno = i.comprador
-JOIN festa f ON i.festa = f.id
-JOIN pessoa p ON i.comprador = p.cpf
+FROM ingresso AS i JOIN cursando AS c ON c.aluno = i.comprador
+JOIN festa AS f ON i.festa = f.id
+JOIN pessoa AS p ON i.comprador = p.cpf
 WHERE DATE_PART('day', NOW() - f.data_horario) <= 180
 GROUP BY p.nome, c.curso
 ORDER BY COUNT(i.comprador) DESC;
@@ -12,14 +12,15 @@ ORDER BY COUNT(i.comprador) DESC;
 -- QUERY 2
 SELECT o.professor, COUNT(DISTINCT o.aluno) as qte_alunos,
 ROUND(AVG(DATE_PART('YEAR', NOW()) - (DATE_PART('YEAR', p.nascimento)))) as idade_media
-FROM orienta o JOIN presenca_marcada pm
-ON o.aluno = pm.aluno JOIN pessoa p
+FROM orienta AS o JOIN presenca_marcada AS pm
+ON o.aluno = pm.aluno JOIN pessoa AS p
 ON o.aluno = p.cpf
 GROUP BY o.professor;
 
 
 -- QUERY 3
-SELECT COUNT(c.locatario) as qte_moradores FROM festa f JOIN contrato_aluguel c
+SELECT COUNT(c.locatario) AS qte_moradores 
+FROM festa AS f JOIN contrato_aluguel AS c
 ON c.residencia = f.moradia
 WHERE c.inicio < DATE(f.data_horario) AND c.fim > DATE(f.data_horario)
 AND f.nome = 'Indy Festa';
@@ -31,21 +32,21 @@ AND f.nome = 'Indy Festa';
 -- Checar a n_animais de cada moradia da mesma cidade do campus e
 -- aceita_animais de cada imóvel que fica na mesma cidade (`true` será
 -- considerado 1 animal e `false`, 0).
-SELECT C.nome_universidade AS UNIVERSIDADE, C.nome_campus AS CAMPUS,
-       COALESCE(SUM(n_animais), 0) + COUNT(aceita_animais) AS NUMERO_ANIMAIS
-FROM residencia AS R
-INNER JOIN campus as C on C.cidade = R.cidade
-LEFT JOIN moradia AS M ON M.id = R.id
-LEFT JOIN imovel AS I ON I.id = R.id AND I.aceita_animais = true
+SELECT c.nome_universidade AS universidade, c.nome_campus AS campus,
+       COALESCE(SUM(n_animais), 0) + COUNT(aceita_animais) AS numero_animais
+FROM residencia AS r
+INNER JOIN campus as c on c.cidade = r.cidade
+LEFT JOIN moradia AS m ON m.id = r.id
+LEFT JOIN imovel AS i ON i.id = r.id AND i.aceita_animais = true
 WHERE n_animais > 0 OR aceita_animais
-GROUP BY R.cidade, C.nome_campus, C.nome_universidade
+GROUP BY r.cidade, c.nome_campus, c.nome_universidade
 ORDER BY coalesce(SUM(n_animais), 0) + COUNT(aceita_animais) DESC;
 
 
 -- QUERY 5
 SELECT ROUND(AVG(DATE_PART('YEAR', NOW()) - (DATE_PART('YEAR', p.nascimento)))) AS idade_media
-FROM aluno a1 JOIN cursando c1 ON a1.cpf = c1.aluno
-JOIN pessoa p ON a1.cpf = p.cpf
+FROM aluno AS a1 JOIN cursando AS c1 ON a1.cpf = c1.aluno
+JOIN pessoa AS p ON a1.cpf = p.cpf
 WHERE (a1.procurando_moradia OR a1.procurando_imovel)
 AND c1.ano_ingresso IN
 (SELECT MIN(ano_ingresso) AS mais_velho FROM aluno a2 JOIN cursando c2
@@ -54,7 +55,7 @@ WHERE a2.procurando_moradia OR a2.procurando_imovel);
 
 
 -- QUERY 6
-SELECT c.locatario as cpf FROM contrato_aluguel c JOIN moradia m
+SELECT c.locatario as cpf FROM contrato_aluguel AS c JOIN moradia AS m
 ON m.id = c.residencia
 WHERE c.locatario NOT IN
 ((SELECT i.comprador FROM ingresso i)
